77ae5ce48ed6d8c6fe6a4682a23cecee77f5fddf
Arturo Alvarado <aalva058@ucr.edu>
2023-09-12 23:13:46 -0700

    Create WhileLoopExercises.java

---
+++ Unit-4/Loops/WhileLoopExercises.java
@@ -0,0 +1,30 @@
+/*
+  (Easy)Countdown Timer:
+  Write a program that takes a positive integer as input and counts down 
+  from that number to 1, printing each number along the way. Print a 
+  surprise message at the end.
+
+  (Medium) Exercise 1: Factorial
+  Create a program that calculates the factorial of a positive integer 
+  input by the user. Use a while loop to calculate the factorial.
+
+
+  (Hard)Exercise 2: FizzBuzz 
+  Implement the FizzBuzz problem using a while loop. Print numbers from 1 
+  to 100, but replace multiples of 3 with "Fizz" and multiples of 5 with 
+  "Buzz." For numbers that are multiples of both 3 and 5, print "FizzBuzz."
+
+  (RIP) Pattern Printing:
+  Write a program that prints the following pattern using a while loop(s):
+  " 
+  " " 
+  " " " 
+  " " " " 
+  " " " " " 
+  " " " " " " 
+  " " " " " " " 
+  " " " " " " " " 
+  " " " " " " " " " 
+  " " " " " " " " " " 
+
+ */
