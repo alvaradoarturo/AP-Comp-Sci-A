21eca835564e0e061e6f5904047118aa68638c23
Arturo Alvarado <aalva058@ucr.edu>
2023-09-20 23:42:28 -0700

    Create IntroToForLoops.java

---
+++ Unit-4/Loops/For Loop/Intro.java
@@ -0,0 +1,89 @@
+/**
+ * Unit3Review
+ */
+public class Unit3Review {
+
+  public static void main(String[] args) {
+    for (int k = 20; k >= 15; k--) {
+      //System.out.print(k + " "); // 20 19 18 17 16 15
+    }
+  }
+}
+/*
+Iterations Cont'd
+-----------------
+The for loop
+
+The general form of the for loop is
+  for(intialization ; termination condition ; update statement){
+    statements // body of loop
+  }
+  The termination condition is tested at the top of the loop;
+  the update statement is performed at the bottom
+  * Loop variable should not change inside the loop body
+  * Initialization and update statements can use any valid constants, variables, or 
+    expressions
+
+  
+  1.
+    for(int i = 1; i < 5; i++){
+      System.out.print(i + " ");
+    }
+  
+    2. 
+    for(int k = 20; k>= 15; k--){
+      System.out.print(k + " ");  
+    } 
+
+    3. 
+    int j;
+    for(j = 2; j <= 10; j+=2){
+      System.out.print(j +  " ");
+    }
+    4. 
+    for(int i = 1; i <=0; i++){
+      System.out.print(i + " ");
+    }
+ */
+/*
+  For Loop 
+  ---------
+  Exercise 0:
+  Get input from the user. Print from 1 to that number.
+  -
+  -
+  -
+  -
+  -
+  Exercise 1:
+  Get input from the user. Print from that number to 1;
+    -
+    -
+    -
+    -
+    -
+    -
+  Exercise 2:
+  Get input from the user. Print every odd number from 1 to that number.
+
+  -
+  -
+  -
+  -
+  -
+  -
+  -
+  Exercise 3:
+  Get input from the user. Print the sum from 1 to that number;
+  -
+  -
+  -
+  -
+  -
+  -
+  -
+  -
+  Exercise 4:
+  Get input from the user. Print the factorial for that number.
+  Ex: input = 4 Ouput: 24
+ */
