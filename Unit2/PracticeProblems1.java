825727f9a20f1f111421e5f082f3b70c87db7d9c
Arturo Alvarado <aalva058@ucr.edu>
2023-10-10 23:38:32 -0700

    Create PracticeProblems1.java

---
+++ Unit2/PracticeProblems1.java
@@ -0,0 +1,28 @@
+/*
+Defining a class 
+  1. first name the class, 
+  2. declares private fields, 
+  3. defines public member methods
+  * methods of the class can access all private member variable*
+ */
+
+/*
+
+  Exercise 0:
+  Create a class called Restaurant. Class should contain variables 
+  for name and rating. Create a functions that allows the user to 
+  set the restaurant's name and rating. Lastly, create a function 
+  that prints name and rating on one line
+
+
+
+  Exercise 1:
+  Create a class called RunnerInfo. The RunnerInfo class maintains 
+  information about a person who runs, allowing a class user to set 
+  the time run(seconds) and the distance run(miles), and to get the 
+  runner's speed(miles/hr). 
+  
+  Examples output:
+  System.out.println("Runner1's speed in MPH: " + _____);
+  System.out.println("Runner2's speed in MPH: " + _____);
+  */
